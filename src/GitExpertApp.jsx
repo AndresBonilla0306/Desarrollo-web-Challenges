ac45ad9365b24d56bfaf74a93eb1a1656cc487eb
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-03-03 14:10:06 -0500

    Challenge6

--- src/GitExpertApp.jsx
+++ src/GitExpertApp.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import {AddCategory} from "./components/AddCategory"
 import {GifGrid} from './components/GifGrid'
+import { useCounter } from './hooks/useCounter'
 //import { useCounter } from './hooks/useCounter'
 const categories = ['empanada', 'hamborguesa']
 
  
 
 export const GitExpertApp = () =>{
+
+    const {counter, increment, decrement, reset} = useCounter(0);
 
     const [categories, setCategories] = useState(['Comida']) 
 
@@ -16,6 +19,11 @@ export const GitExpertApp = () =>{
     
     return(
         <>
+        <h1>Contador</h1>
+        <span>Me comí estás salchipapas: {counter}</span><br></br>
+        <button onClick={() => increment() }> +1 </button>
+        <button onClick = {()=> decrement()}>-1</button>
+        <button onClick = {()=> reset()}>Reiniciar</button>
          <h1>GitExpert</h1> 
         <AddCategory onAddCategory ={onAddCategory}/>
         {
--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-export const useCounter = ({ value }) => { 
+export const useCounter = (value) => { 
 
     const  [counter, setCounter] = useState( value )
   
@@ -20,21 +20,26 @@ export const useCounter = ({ value }) => {
       console.log('llamando handleReset')
       setCounter(value * 0)
   }
-    return (
-      <>
+  
+    return {
+        counter,
+        increment: handleAdd,
+        decrement: handleSubstract,
+        reset: handleReset,
+      /*<>
           <h1>Contador</h1>
           <span> { counter } </span><br></br>
           <button onClick={() => handleAdd() }> +1 </button>
           <button onClick = {()=> handleSubstract()}>-1</button>
           <button onClick = {()=> handleReset()}>Reiniciar</button>
-      </>
-    )
+      </>*/
+    }
   }
   
-  useCounter.proptypes = {
+  /*useCounter.proptypes = {
     value: PropTypes.number.isRequired
   }
   useCounter.defaultProps = {
     value: 1
-  }
+  }¨*/
   
