1ee103672974d992bf64576acf775317e375234c
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-03-03 10:42:32 -0500

    Challenge2

---
+++ ChallengeTwo/src/FirstApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Titulo = 'Soy Bataman'
+const FirstApp = () => {
+  return (
+    <div>
+        <h1>{Titulo}</h1>
+        <span>10</span>
+    </div>
+  )
+}
+
+export default FirstApp
