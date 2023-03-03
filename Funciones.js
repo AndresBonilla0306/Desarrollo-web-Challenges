27cc459792079122772931874f8fbe9f04cc3556
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-03-03 10:22:22 -0500

    Challenge 1

---
+++ Funciones.js
@@ -0,0 +1,13 @@
+//Funciones
+
+const number = prompt("Ingresa un numero: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("El numero es par.");
+}
+
+// if the number is odd
+else {
+    console.log("El numero es impar.");
+}
