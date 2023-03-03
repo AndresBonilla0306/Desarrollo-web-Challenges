c03548743a4f05054b10ce82686b06663ede188f
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-03-03 11:33:05 -0500

    Challenge4

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import FirstApp from './FirstApp'
+//import FirstApp from './FirstApp'
+import GitExpertApp from './GitExpertApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <FirstApp/>
+    <GitExpertApp/>
   </React.StrictMode>,
 )
