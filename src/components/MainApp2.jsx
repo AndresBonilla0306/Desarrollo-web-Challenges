40b0e1e469134452c59a2a788d69626209aede96
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-04-22 09:23:56 -0500

    Challenge14

--- src/components/MainApp2.jsx
+++ src/components/MainApp2.jsx
@@ -5,7 +5,7 @@ import { Homepage } from './Homepage'
 export const MainApp2 = () => {
   return (
     <>
-        <h1>MainApp</h1>
+        <h1>Fake Login</h1>
         <NavComponent/>
         <hr/>
     </>
