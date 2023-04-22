80899c544e66f5f19e457c4a40abc385a48d398f
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-04-21 23:08:41 -0500

    Challenge13

--- src/components/GetItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const GetItem = ({title, url}) => {
-  return (
-    <div className='Card'>
-        <img src={url} alt={title}/>
-    </div>
-  )
-}
--- src/components/MainApp2.jsx
+++ src/components/MainApp2.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
-import { NavComponent } from './NavComponent'
-import { Homepage } from './Homepage'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from '../Store/slices/CounterSlice'
+import { decrement } from '../Store/slices/CounterSlice'
 
 export const MainApp2 = () => {
+
+  const { counter } = useSelector ( state => state.counter);
+  const  dispatch = useDispatch();
+
   return (
     <>
         <h1>MainApp</h1>
-        <NavComponent/>
         <hr/>
+        <span> Counter is: {counter}</span>
+        <button
+          className='btn btn-primary'
+          onClick={ () => dispatch( increment() )}
+        > +1 </button>
+        <button
+          className='btn btn-primary'
+          onClick={ () => dispatch (decrement())}
+          > -1</button>
     </>
   )
 }
